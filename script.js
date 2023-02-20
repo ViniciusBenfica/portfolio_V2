af888dbbff7633473945ee5e1925cf9a1d1b643c
Vinicius <vinicius.benfica.ruy@gmail.com>
2023-02-20 13:41:35 -0300

    start section findMe

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const skills = [
-    {name: "html", porcentagem: "84%"},
+    { name: "html", porcentagem: "84%" },
     // {name: "next", porcentagem: "10%"},
-    {name: "vue", porcentagem: "50%"},
-    {name: "react", porcentagem: "90%"},
-    {name: "nest", porcentagem: "68%"},
-    {name: "sass", porcentagem: "50%"},
-    {name: "node", porcentagem: "65%"},
+    { name: "vue", porcentagem: "50%" },
+    { name: "react", porcentagem: "90%" },
+    { name: "nest", porcentagem: "68%" },
+    { name: "sass", porcentagem: "50%" },
+    { name: "node", porcentagem: "65%" },
     // {name: "javascript", porcentagem: "50%"},
-    {name: "typescript", porcentagem: "50%"},
-    {name: "css", porcentagem: "85%"},
+    { name: "typescript", porcentagem: "50%" },
+    { name: "css", porcentagem: "85%" },
 ]
 
 skills.map((item) => {
