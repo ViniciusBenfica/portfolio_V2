6e2dc4aa5063a1764fb3fc1f3100b1fabc08b6b8
Vinicius <vinicius.benfica.ruy@gmail.com>
2023-03-25 15:15:55 -0300

    style section about

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const sections = document.querySelectorAll('.home, .about');
-const journey = document.querySelector('.journey');
-const teste = document.querySelector('.about');
 
 const changeURLSection = (section) => {
     history.pushState(null, null, `/index.html#${section.id}`);
@@ -15,12 +13,6 @@ window.addEventListener('scroll', () => {
 
         if (scrollPos >= sectionTop - sectionHeight / 2 && scrollPos < sectionTop + sectionHeight / 2) {
             changeURLSection(section)
-
-          /*   if (section.id === "about") {
-                journey.style.transform = `translateY(${normalizedScrollPos / containerHeight * 100}%)`;
-            } else {
-                journey.style.transform = `translateY(${0}px)`;
-            } */
 
         }
     });
