6b3c250837e06c736917a5d5aadfd433a3b7211a
Vinicius <vinicius.benfica.ruy@gmail.com>
2023-04-10 22:50:16 -0300

    add button and change fonts

--- script.js
+++ script.js
@@ -26,3 +26,21 @@ window.addEventListener('scroll', () => {
 
 centerJourney();
 window.addEventListener('resize', centerJourney);
+
+
+var animateButton = function(e) {
+
+    e.preventDefault;
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
