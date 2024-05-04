a9f3ea404c09ae12d3f74cd70b3c053712e1fed6
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-05-04 18:09:41 -0300

    ♻️ refacture code

--- web/script.js
+++ web/script.js
@@ -1,57 +1,65 @@
-const sections = document.querySelectorAll('.home, .about, .findMe, .technologies');
+const sections = document.querySelectorAll(
+	".home, .about, .findMe, .technologies",
+);
 
 const changeURLSection = (section) => {
-    history.pushState(null, null, `/index.html#${section.id}`);
-}
+	history.pushState(null, null, `/index.html#${section.id}`);
+};
 
 const centerJourney = () => {
-    const journey = document.querySelector('.journey');
-    const heightJourney = journey.offsetHeight;
-    journey.style.top = `calc(50% - ${heightJourney / 2}px)`;
-}
+	const journey = document.querySelector(".journey");
+	const heightJourney = journey.offsetHeight;
+	journey.style.top = `calc(50% - ${heightJourney / 2}px)`;
+};
 
-window.addEventListener('scroll', () => {
-    const scrollPos = window.scrollY;
+window.addEventListener("scroll", () => {
+	const scrollPos = window.scrollY;
 
-    sections.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.offsetHeight;
+	for (const section of sections) {
+		const sectionTop = section.offsetTop;
+		const sectionHeight = section.offsetHeight;
 
-        if (scrollPos >= sectionTop - sectionHeight / 2 && scrollPos < sectionTop + sectionHeight / 2) {
-            changeURLSection(section)
-
-        }
-    });
+		if (
+			scrollPos >= sectionTop - sectionHeight / 2 &&
+			scrollPos < sectionTop + sectionHeight / 2
+		) {
+			changeURLSection(section);
+		}
+	}
 });
 
 centerJourney();
-window.addEventListener('resize', centerJourney);
+window.addEventListener("resize", centerJourney);
 
-var animateButton = function (e) {
+const animateButton = (e) => {
+	e.preventDefault;
+	e.target.classList.remove("animate");
 
-    e.preventDefault;
-    e.target.classList.remove('animate');
-
-    e.target.classList.add('animate');
-    setTimeout(function () {
-        e.target.classList.remove('animate');
-    }, 700);
+	e.target.classList.add("animate");
+	setTimeout(() => {
+		e.target.classList.remove("animate");
+	}, 700);
 };
 
-document.getElementById("bubbly-button").addEventListener('mouseenter', animateButton, false)
+document
+	.getElementById("bubbly-button")
+	.addEventListener("mouseenter", animateButton, false);
 
-document.getElementById('change-language').addEventListener('click', function() {
-    let currentLang = this.src.includes('Brasil.webp') ? 'en' : 'pt';
-    switchLanguage(currentLang);
-    this.src = currentLang === 'en' ? './img/USA.webp' : './img/Brasil.webp';
-});
+document
+	.getElementById("change-language")
+	.addEventListener("click", function () {
+		const currentLang = this.src.includes("Brasil.webp") ? "en" : "pt";
+		switchLanguage(currentLang);
+		this.src = currentLang === "en" ? "./img/USA.webp" : "./img/Brasil.webp";
+	});
 
 function switchLanguage(lang) {
-    document.querySelectorAll("[data-key]").forEach(elem => {
-        let key = elem.getAttribute("data-key");
-        if (languages[lang][key]) {
-            console.log(elem.textContent)
-            elem.textContent = languages[lang][key];
-        }
-    });
+	const elements = document.querySelectorAll("[data-key]");
+	for (const elem of elements) {
+		const key = elem.getAttribute("data-key");
+		if (languages[lang][key]) {
+			console.log(elem.textContent);
+			elem.textContent = languages[lang][key];
+		}
+	}
 }
